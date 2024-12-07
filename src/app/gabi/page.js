0b2b8cb63178dd804d397125bebeced7a61cc01c
reikuyao <lxnnylqs44@gmail.com>
2024-12-07 23:04:43 +0100

    fix

---
+++ src/app/gabi/page.js
@@ -0,0 +1,16 @@
+export const viewport = {
+	themeColor: '#ff5353',
+};
+
+export const metadata = {
+  	title: "GAB EL KAKATO",
+  	description: "Nobody Loves Me Like You",
+};
+
+export default function Gabi() {
+  	return (
+    	<main>
+      		<div>Gbibi x)</div>
+    	</main>
+  	);
+}
