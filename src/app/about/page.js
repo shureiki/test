515bb6e96b71faf816b91422eed74503711bc89f
reikuyao <lxnnylqs44@gmail.com>
2024-12-07 22:27:12 +0100

    First commit

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "GAB EL K",
+  description: "Nobody Loves Me Like You",
+};
+
+export default function About() {
+  return (
+    <main>
+      <div>About hihi</div>
+    </main>
+  );
+}
