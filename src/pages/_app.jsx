e68119dd20a3f6f04fcf43d0775779fca4b938a6
shureiki <lxnnylqs44@gmail.com>
2024-12-20 19:18:12 +0100

    App Router => Page Router

---
+++ src/pages/_app.jsx
@@ -0,0 +1,7 @@
+import './globals.css';
+
+export const App = ({ Component, pageProps }) => {
+    return <Component {...pageProps} />
+}
+
+export default App;
---
+++ src/pages/test.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export const Test = () => {
+    return (
+        <div>
+            <Head>
+                <meta name='title' content='TEST !!!' />
+                <meta name='theme-color' content='#00FF00' />
+            </Head>
+
+            <p>TEST PAGE !!!</p>
+        </div>
+    )
+}
+
+export default Test;
