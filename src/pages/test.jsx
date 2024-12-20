011d913c6e1f3f39fced53a9656c1549b4c27a5a
shureiki <lxnnylqs44@gmail.com>
2024-12-20 19:21:57 +0100

    test

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -4,8 +4,10 @@ export const Test = () => {
     return (
         <div>
             <Head>
-                <meta name='title' content='TEST !!!' />
-                <meta name='theme-color' content='#00FF00' />
+                <title>TEST !!!</title>
+
+                <meta name='title' content='TEST !!!' key='title' />
+                <meta name='theme-color' content='#00FF00' key='theme-color' />
             </Head>
 
             <p>TEST PAGE !!!</p>
