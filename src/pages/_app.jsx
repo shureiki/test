862497a95fd9cad002294f5027d20e36b379d6de
shureiki <lxnnylqs44@gmail.com>
2024-12-20 19:48:16 +0100

    fix

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,15 +1,13 @@
-import Head from 'next/head';
 import './globals.css';
+
+import Head from 'next/head';
 
 export const App = ({ Component, pageProps }) => {
     return (
         <>
             <Head>
                 <title>Portfolio</title>
-                
-                <meta name='title' content='Portfolio' key='title' />
-                <meta name='description' content='A portfolio' key='description' />
-                <meta name='theme-color' content='#4285f4' key='theme-color' />
+                <meta name='title' content='Portfolio' />
             </Head>
 
             <Component {...pageProps} />
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,19 @@
+import Head from 'next/head'
+
 export const Page = () => {
-    return <h1>Hello, Next.js!</h1>
+    return (
+        <div>
+            <Head>
+                <title>PORTFOLIO TAMAMAN HIHI</title>
+                
+                <meta name='title' content='PORTFOLIO TAMAMAN' />
+                <meta name='description' content='Test page tamaman' />
+                <meta name='theme-color' content='#FF00FF' />
+            </Head>
+
+            <h1>Hello, Next.js!</h1>
+        </div>
+    )
 }
 
 export default Page; 
